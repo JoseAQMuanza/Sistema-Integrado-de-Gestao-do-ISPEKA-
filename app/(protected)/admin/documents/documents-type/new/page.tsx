4c6c12dfa841f1d7e887deef95db76f4c717ade9
JoseAQMuanza <muanzajoseantonio2@gmail.com>
2026-01-05 20:04:58 +0100

    corrigindo erro em /documents-type/new

--- app/(protected)/admin/documents/documents-type/new/page.tsx
+++ app/(protected)/admin/documents/documents-type/new/page.tsx
@@ -1,3 +1,7 @@
-export default function DocumentsTypeNewPage() {
-  return <h1>Em Desenvolvimento</h1>
+export default function TypeDocumentNewPage() {
+  return (
+    <div className="bg-white rounded-lg shadow p-6 mb-8">
+      <h2 className="text-lg font-semibold text-gray-900 mb-4">Em Desenvolvimento</h2>      
+    </div>
+  )
 }
